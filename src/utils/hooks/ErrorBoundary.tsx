f7261e0f35d4420fd43bb181a2d6c9fa4c4d90b1
HANTAEDONG <htd0913@gmail.com>
2024-11-15 16:55:40 +0900

    [Feat] Error Boundary 추가

---
+++ src/utils/hooks/ErrorBoundary.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(_error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
