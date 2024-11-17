01a79807b518723a111797e4f0a8519440e43723
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-18 01:26:42 +0900

    [Feat] 라우터에 레이아웃, 회원가입, 로그인 페이지 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,26 +1,33 @@
 import HomePage from "@/pages/HomePage";
 import LoginPage from "@/pages/LoginPage";
-import SignUpPage from "@/pages/SignupPage";
+import SignUpPage from '@/pages/SignupPage';
 import Bottom from "@/ui/components/Bottom";
+import AuthLayout from '@/ui/components/layout/AuthLayout';
 
 const router = [
   {
-    path: "/",
+    path: '/',
     element: <Bottom />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <HomePage />,
       },
     ],
   },
   {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/signup",
-    element: <SignUpPage />,
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'signup',
+        element: <SignUpPage />,
+      },
+    ],
   },
 ];
 
