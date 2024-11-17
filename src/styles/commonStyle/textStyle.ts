31b829fe11c877a504d8a3779d1966e597b4acfe
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-18 01:22:09 +0900

    [Feat] 텍스트 스타일 및 버튼 cva 작성 및 적용

---
+++ src/styles/commonStyle/textStyle.ts
@@ -0,0 +1,67 @@
+import { defineTextStyles } from '@pandacss/dev';
+
+export const textStyles = defineTextStyles({
+	heading1: {
+		value: {
+			fontFamily: 'GmarketSans',
+			fontWeight: 'bold',
+			fontSize: '25px',
+		},
+	},
+	heading2: {
+		value: {
+			fontFamily: 'GmarketSans',
+			fontWeight: 'medium',
+			fontSize: '22px',
+		},
+	},
+	body1: {
+		value: {
+			fontFamily: 'GmarketSans',
+			fontWeight: 'medium',
+			fontSize: '16px',
+		},
+	},
+	body2: {
+		value: {
+			fontFamily: 'GmarketSans',
+			fontWeight: 'medium',
+			fontSize: '13px',
+		},
+	},
+	body3: {
+		value: {
+			fontFamily: 'GmarketSans',
+			fontWeight: 'light',
+			fontSize: '13px',
+		},
+	},
+	caption1: {
+		value: {
+			fontFamily: 'GmarketSans',
+			fontWeight: 'medium',
+			fontSize: '12px',
+		},
+	},
+	caption2: {
+		value: {
+			fontFamily: 'GmarketSans',
+			fontWeight: 'medium',
+			fontSize: '10px',
+		},
+	},
+	button1: {
+		value: {
+			fontFamily: 'GmarketSans',
+			fontWeight: 'bold',
+			fontSize: '16px',
+		},
+	},
+	button2: {
+		value: {
+			fontFamily: 'GmarketSans',
+			fontWeight: 'bold',
+			fontSize: '14px',
+		},
+	},
+});
