8dccc6260509c16428dfcf5a250cfc31cff6aef5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-18 01:36:16 +0900

    [Feat] 출력 오류로 인해 BrowserRouter 추가

--- src/ui/view/molecule/Header.stories.tsx
+++ src/ui/view/molecule/Header.stories.tsx
@@ -1,9 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Header from './Header';
+import { BrowserRouter } from 'react-router-dom';
 
 const meta: Meta<typeof Header> = {
   title: 'Components/Molcules/Header',
   component: Header,
+  decorators: [
+    Story => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
 };
 
 export default meta;
@@ -12,7 +20,7 @@ type Story = StoryObj<typeof Header>;
 export const Default: Story = {};
 
 export const Text: Story = {
-	args: {
-		headerText: '로그인'
-	}
+  args: {
+    headerText: '로그인',
+  },
 };
