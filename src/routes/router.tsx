a99f3cf863fb25fa411614964255eb91704f4d2e
HANTAEDONG <htd0913@gmail.com>
2024-11-17 09:24:44 +0900

    [Refactor] router와 Bottom 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,26 @@
-import HomePage from "../pages/HomePage";
+import HomePage from "@/pages/HomePage";
+import LoginPage from "@/pages/LoginPage";
+import SignUpPage from "@/pages/SignupPage";
+import Bottom from "@/ui/components/Bottom";
 
 const router = [
   {
     path: "/",
-    element: <HomePage />,
+    element: <Bottom />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />,
   },
 ];
 
