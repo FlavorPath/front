36b03c56e2f797ada7f68ceec3cfff3bf24084b2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-18 01:25:26 +0900

    [Feat] 로그인/회원가입 페이지의 공통 스타일 작성

---
+++ src/styles/auth.styles.ts
@@ -0,0 +1,43 @@
+import { css } from '@styled-system/css';
+import { center, flex } from '@styled-system/patterns';
+
+export const authStyles = {
+  error: css({
+    borderBottomColor: 'secondary.red',
+    borderBottomWidth: '1px',
+  }),
+  error_text: css({
+    color: 'secondary.red',
+    textStyle: 'body2',
+    marginTop: '8px',
+  }),
+  form: css({
+    display: 'flex',
+    flexDir: 'column',
+    margin: '0 30px',
+    gap: '46px',
+  }),
+  link_text: css({
+    textAlign: 'center',
+    textStyle: 'caption1',
+    fontWeight: 'light',
+    marginTop: '16px',
+  }),
+  button_grp: css({
+    width: 'calc(100% - 60px)',
+    position: 'absolute',
+    bottom: '128px',
+    left: 0,
+    margin: '0 30px'
+  }),
+  input_wrap: flex({
+    flexDirection: 'column',
+    gap: '54px',
+    margin: '0 30px'
+  }),
+  logo_box: center({
+    width: '375px',
+    height: '214px',
+    marginBottom: '30px',
+  }),
+};
