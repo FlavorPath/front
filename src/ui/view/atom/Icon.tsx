ea4c30d09b51ed2d70a892dbd1985fa8701c286d
HANTAEDONG <htd0913@gmail.com>
2024-11-18 15:16:54 +0900

    [Refactor] Icon에 fill 속성도 추가

--- src/ui/view/atom/Icon.tsx
+++ src/ui/view/atom/Icon.tsx
@@ -1,21 +1,19 @@
-import * as HeroIcon from 'react-icons/hi';
+import * as HeroIcon from "react-icons/hi";
 
 interface IProps {
   iconName: keyof typeof HeroIcon;
   size?: number;
   color?: string;
   onClick: () => void;
-	className?: string;
+  className?: string;
+  fill?: string;
 }
 
-const Icon = ({ iconName, size, color, onClick, className }: IProps) => {
-	const IconComponent = HeroIcon[iconName];
+const Icon = ({ iconName, size, color, onClick, className, fill }: IProps) => {
+  const IconComponent = HeroIcon[iconName];
   return (
     <button type="button" onClick={onClick} className={className}>
-      <IconComponent
-        size={size}
-        color={color}
-      />
+      <IconComponent size={size} color={color} fill={fill} />
     </button>
   );
 };
