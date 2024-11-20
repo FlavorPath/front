3411209c70c1bf9aa3c8b5dfd733db73152b6322
HANTAEDONG <htd0913@gmail.com>
2024-11-20 15:52:02 +0900

    [Feat] 맵 마커 라벨에 맞게 불러오기 구현

---
+++ src/ui/view/atom/CustomMapMarker.tsx
@@ -0,0 +1,60 @@
+import { MapMarker } from "react-kakao-maps-sdk";
+import KoreanMarker from "@/assets/KoreanFood.svg";
+import JapaneseMarker from "@/assets/JapaneseFood.svg";
+import RamenMarker from "@/assets/Ramen.svg";
+import ChineseMarker from "@/assets/ChineseFood.svg";
+import WesternMarker from "@/assets/WesternFood.svg";
+import useBottomSheetStore from "@/store/stores/BottomSheet.store";
+
+const Label = ["Korean", "Japanese", "Ramen", "Chinese", "Western"] as const;
+type Label = (typeof Label)[number];
+
+interface ILocation {
+  latitude: number;
+  longitude: number;
+}
+
+interface IProp {
+  location: ILocation;
+  restaurantId: number;
+  name: string;
+  label: Label[];
+}
+
+const markerMap = {
+  Korean: KoreanMarker,
+  Japanese: JapaneseMarker,
+  Ramen: RamenMarker,
+  Chinese: ChineseMarker,
+  Western: WesternMarker,
+} as const;
+
+const CustomMapMarker = ({ location, restaurantId, name, label }: IProp) => {
+  const setOpenBottomSheet = useBottomSheetStore(
+    (state) => state.setOpenBottomSheet
+  );
+  const onClick = () => {
+    console.log(`식당이름: ${name}`);
+    setOpenBottomSheet(true);
+  };
+
+  return (
+    <MapMarker
+      position={{
+        lat: location.latitude,
+        lng: location.longitude,
+      }}
+      image={{
+        src: markerMap[label[0]],
+        size: {
+          width: 34,
+          height: 43,
+        },
+      }}
+      title={name}
+      onClick={onClick}
+    />
+  );
+};
+
+export default CustomMapMarker;
