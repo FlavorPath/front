fdf168662565139ebab733f0b431d8d167bdad00
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-21 18:23:18 +0900

    [Feat] 검색에 대한 스토어 구현

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,15 +1,18 @@
+import useSearchStore from '@/store/stores/search.store';
 import useStoreState from '@/store/stores/stores.store';
 import useToggleStore from '@/store/stores/toggle.store';
+import SearchInput from '@/ui/view/atom/SearchInput';
 import Toggle from '@/ui/view/atom/Toggle';
 import Header from '@/ui/view/molecule/Header';
 import StoreListTemplate from '@/ui/view/templates/StoreListTemplate';
 import { css } from '@styled-system/css';
-import { flex } from '@styled-system/patterns';
+import { center, flex } from '@styled-system/patterns';
 import { useEffect } from 'react';
 
 const SearchPage = () => {
 	const { isOn } = useToggleStore();
 	const { setStores, setNoResultText } = useStoreState();
+	const { searchValue, setSearchValue } = useSearchStore();
 
 	useEffect(() => {
     const fetchedStores = [{storeName: '테스트', storeAddress: '테스트주소'}];
@@ -20,6 +23,15 @@ const SearchPage = () => {
 	return (
     <div>
       <Header headerText='탐색' />
+      <div className={styles.input_box}>
+        <SearchInput
+          icon='MagnifyingGlassIcon'
+          placeholder='식당을 탐색해보세요'
+          value={searchValue}
+					onValueChange={setSearchValue}
+					autoFocus
+        />
+      </div>
       <div className={styles.toggle_wrap}>
         <div className={styles.toggle_text}>음식</div>
         <Toggle />
@@ -35,10 +47,13 @@ const styles = {
   toggle_wrap: flex({
     padding: '8px 30px',
     alignItems: 'center',
-		gap: '8px',
-		fontFamily: 'Gmarket Sans'
+    gap: '8px',
+    fontFamily: 'Gmarket Sans',
   }),
   toggle_text: css({
     textStyle: 'body1',
   }),
+	input_box: center({
+		padding: '8px 0'
+	}),
 };
