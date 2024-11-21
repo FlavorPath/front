2f664a9cf8d7e7df6b722daea102250e927d8fe7
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-21 22:19:21 +0900

    [Feat] 인풋 코드 수정

--- src/ui/view/atom/SearchInput.tsx
+++ src/ui/view/atom/SearchInput.tsx
@@ -1,7 +1,7 @@
 import { InputHTMLAttributes } from "react";
 import * as HeroSolidIcon from "@heroicons/react/24/solid";
 import * as HeroOutlineIcon from "@heroicons/react/24/outline";
-import { css } from "@styled-system/css";
+import { css, cx } from "@styled-system/css";
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
@@ -32,19 +32,22 @@ const SearchInput = ({
   };
 
   return (
-    <div className={`${styles.inputWrap} ${className || ""}`}>
+    <div className={cx(styles.inputWrap, className)}>
       <input
         {...props}
-        type="text"
+        type='text'
         defaultValue={defaultValue}
         value={value}
         onChange={handleChange}
         disabled={disabled}
-        className={styles.input}
+        className={cx(styles.input, !props.readOnly && styles.hidePlaceholder)}
       />
       {IconComponent && (
         <span className={styles.icon}>
-          <IconComponent width={20} height={20} />
+          <IconComponent
+            width={20}
+            height={20}
+          />
         </span>
       )}
     </div>
@@ -55,31 +58,36 @@ export default SearchInput;
 
 const styles = {
   inputWrap: css({
-    display: "flex",
-    alignItems: "center",
-    width: "315px",
-    height: "40px",
-    border: "2px solid",
-    borderColor: "primary.main",
-    borderRadius: "8px",
-    padding: "0 10px",
-    backgroundColor: "white",
-    overflow: "hidden",
+    display: 'flex',
+    alignItems: 'center',
+    width: '315px',
+    height: '40px',
+    border: '2px solid',
+    borderColor: 'primary.main',
+    borderRadius: '8px',
+    padding: '0 10px',
+    backgroundColor: 'white',
+    overflow: 'hidden',
   }),
   icon: css({
-    marginRight: "8px",
-    color: "primary.main",
-    strokeWidth: "2",
+    marginRight: '8px',
+    color: 'primary.main',
+    strokeWidth: '2',
   }),
   input: css({
     flex: 1,
-    border: "none",
-    outline: "none",
-    fontSize: "14px",
-    fontWeight: "bold",
-    color: "gray.700",
-    "&::placeholder": {
-      color: "background.lightgrtay",
+    border: 'none',
+    outline: 'none',
+    fontSize: '14px',
+    fontWeight: 'bold',
+    color: 'gray.700',
+    '&::placeholder': {
+      color: 'background.lightgrtay',
+    },
+  }),
+  hidePlaceholder: css({
+    '&:focus::placeholder': {
+      color: 'white',
     },
   }),
 };
