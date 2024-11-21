f93b87ab5a9f5a2135326b88668ab01f28547484
HANTAEDONG <htd0913@gmail.com>
2024-11-21 19:46:47 +0900

    [Chore] msw 기초작업 완료

--- src/mocks/handlers/index.tsx
+++ src/mocks/handlers/index.tsx
@@ -1,7 +1,8 @@
 import { delay } from "msw";
+import { mapHandlers } from "./map-handler.mock";
 export const delayForDevelopment = async (ms = 1000) => {
   if (process.env.NODE_ENV === "development") {
     await delay(ms);
   }
 };
-export const handlers = [];
+export const handlers = [...mapHandlers];
