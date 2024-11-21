6dea0adac19200bf43c5a2d9ba0684f2b6d7e824
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-21 14:39:18 +0900

    [Feat] 아이콘에 width와 height 추가

--- src/ui/view/atom/Icon.tsx
+++ src/ui/view/atom/Icon.tsx
@@ -5,16 +5,24 @@ interface IProps {
   size?: number;
   color?: string;
   onClick: () => void;
-	className?: string;
+  className?: string;
+  width?: number;
+  height?: number;
 }
 
-const Icon = ({ iconName, size, color, onClick, className }: IProps) => {
-	const IconComponent = HeroIcon[iconName];
+const Icon = ({ iconName, size, color, onClick, className, width, height }: IProps) => {
+  const IconComponent = HeroIcon[iconName];
   return (
-    <button type="button" onClick={onClick} className={className}>
+    <button
+      type='button'
+      onClick={onClick}
+      className={className}
+    >
       <IconComponent
         size={size}
         color={color}
+        width={width || size}
+        height={height || size}
       />
     </button>
   );
