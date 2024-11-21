aaef0c1f8ceea5fd59bd3cd7b99f050f27ab702d
HANTAEDONG <htd0913@gmail.com>
2024-11-21 16:23:05 +0900

    [Feat] map의 msw handler 구현

--- src/mocks/handlers/map-handler.mock.ts
+++ src/mocks/handlers/map-handler.mock.ts
@@ -1,1 +1,22 @@
-export const mapHandlers = [];
+import { API_PATH } from "@/api/api-path";
+import { HttpResponse, http } from "msw";
+import { mapMockData } from "../mock-data/map.mock";
+import { delayForDevelopment } from ".";
+export const mapHandlers = [
+  http.get(API_PATH.marker, async () => {
+    await delayForDevelopment();
+    return HttpResponse.json(mapMockData);
+  }),
+  http.get(API_PATH.marker, async ({ request }) => {
+    const url = new URL(request.url);
+    const label = url.searchParams.get("label");
+    await delayForDevelopment();
+    if (label) {
+      return HttpResponse.json(
+        mapMockData.filter((data) => data.label.includes(label))
+      );
+    }
+    // 라벨 없을 때 전체 데이터 반환
+    return HttpResponse.json(mapMockData);
+  }),
+];
