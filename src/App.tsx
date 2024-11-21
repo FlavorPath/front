b68f85a8625d709935011e89de22392036cf6fc6
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-21 14:39:43 +0900

    [Chore] tanstack query 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import router from "./routes/router";
 import Layout from './ui/components/layout/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const queryClient = new QueryClient();
   const appRouter = createBrowserRouter(router);
 
   return (
-    <Layout>
-      <RouterProvider router={appRouter} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <RouterProvider router={appRouter} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
 
