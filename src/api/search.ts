06c56273cee7c7a4f0ae2f04f7c12c4d7633ebee
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-25 17:30:34 +0900

    [Fix] 검색 API 수정

--- src/api/search.ts
+++ src/api/search.ts
@@ -4,5 +4,5 @@ export const fetchSearchStore = async (searchText: string, isToggleOn: boolean) 
   const url = `/search?toggle=${isToggleOn ? 'label' : 'name'}&query=${encodeURIComponent(searchText)}`;
 
 	const response = await axiosInstance.get(url);
-  return response.data;
+  return response.data.data;
 };
