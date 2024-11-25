ad72e607e39e40f42c88768390f095b3eab7fd56
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-25 14:52:55 +0900

    [Feat] 이미 로그인한 경우 로그인/회원가입에 접근하지 못하게 수정

--- src/ui/components/layout/AuthLayout.tsx
+++ src/ui/components/layout/AuthLayout.tsx
@@ -1,21 +1,25 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Header from "@/ui/view/molecule/Header";
+import useAuth from '@/store/stores/auth.store';
 
-type AuthPathType = "/signup" | "/login";
+type AuthPathType = '/auth/signup' | '/auth/login';
 
 const AuthLayout = () => {
   const location = useLocation();
-  const [type, setType] = useState<AuthPathType>("/login");
-  const text = type === "/signup" ? "회원가입" : "로그인";
+  const navigate = useNavigate();
+  const {accessToken} = useAuth()
+  const [type, setType] = useState<AuthPathType>('/auth/login')
+
+  if (accessToken) navigate('/')
 
   useEffect(() => {
     setType(location.pathname as AuthPathType);
   }, [location.pathname]);
 
   return (
-    <div style={{ width: '100%', height: "100%", position: "relative" }}>
-      <Header headerText={text} />
+    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+      <Header headerText={type === '/auth/signup' ? '회원가입' : '로그인'} />
       <Outlet />
     </div>
   );
