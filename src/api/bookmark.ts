8e54919b5170a1edc3658cd38b51f0715063d431
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-26 16:27:04 +0900

    [Feat] 북마크 조회 시 반환 값 수정

--- src/api/bookmark.ts
+++ src/api/bookmark.ts
@@ -2,7 +2,7 @@ import axiosInstance from '.'
 
 export const fetchGetBookmarks = async () => {
 	const response = await axiosInstance.get('/scrap');
-	return response.data
+	return response.data.data
 }
 
 export const removeBookmarks = async (restaurantId: number) => {
